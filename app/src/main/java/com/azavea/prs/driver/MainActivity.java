b182d6665cf93ef4a502539afbf03a2576979226
Kathryn Killebrew <kkillebrew@azavea.com>
2015-11-19 20:05:52 -0500

    Load a schema using jsonschema2pojo

--- app/src/main/java/com/azavea/prs/driver/MainActivity.java
+++ app/src/main/java/com/azavea/prs/driver/MainActivity.java
@@ -7,7 +7,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.sun.codemodel.CodeWriter;
+import com.sun.codemodel.JCodeModel;
+
+import org.jsonschema2pojo.*;
+import org.jsonschema2pojo.rules.RuleFactory;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import com.azavea.prs.driver.schemas.*;
+
 public class MainActivity extends AppCompatActivity {
+
+
+    ExampleSchema mySchema;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +40,40 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    void buildSchema() {
+
+        JCodeModel codeModel = new JCodeModel();
+
+        /*
+        URL source = null;
+        try {
+            source = new URL("file:///res/values/json/schemas/example_schema.json");
+
+            GenerationConfig config = new DefaultGenerationConfig() {
+                @Override
+                public boolean isGenerateBuilders() { // set config option by overriding method
+                    return true;
+                }
+            };
+
+            SchemaMapper mapper = new SchemaMapper(new RuleFactory(config, new Jackson2Annotator(), new SchemaStore()), new SchemaGenerator());
+            mapper.generate(codeModel, "MySchema", "com.azavea.prs", source);
+
+
+
+            codeModel.build(new File(getFilesDir() + "/MyFooOutput"));
+
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        */
+
+
     }
 
 }
